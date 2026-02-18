8a711412505dc5df2a913b48cbf2e7d299bc5086
thiagoh20 <thiago.bbolivar@gmail.com>
2026-02-18 08:00:58 -0500

    chore: se usa un data source, no un recurso.

--- s3/outputs.tf
+++ s3/outputs.tf
@@ -1,24 +1,24 @@
 output "bucket_id" {
   description = "The ID of the S3 bucket"
-  value       = aws_s3_bucket.frontend.id
+  value       = data.aws_s3_bucket.frontend.id
 }
 
 output "bucket_arn" {
   description = "The ARN of the S3 bucket"
-  value       = aws_s3_bucket.frontend.arn
+  value       = data.aws_s3_bucket.frontend.arn
 }
 
 output "bucket_domain_name" {
   description = "The bucket domain name"
-  value       = aws_s3_bucket.frontend.bucket_domain_name
+  value       = data.aws_s3_bucket.frontend.bucket_domain_name
 }
 
 output "bucket_regional_domain_name" {
   description = "The bucket regional domain name"
-  value       = aws_s3_bucket.frontend.bucket_regional_domain_name
+  value       = data.aws_s3_bucket.frontend.bucket_regional_domain_name
 }
 
 output "origin_access_control_id" {
   description = "The ID of the Origin Access Control"
-  value       = aws_cloudfront_origin_access_control.frontend.id
+  value       = aws_cloudfront_origin_access_control.website.id
 }
